45b1ffb02eaa7e29d068daf92b7bedf0ea545826
Becca <beccaburns89@gmail.com>
2019-09-24 09:41:19 +0200

    Creates feature file for creating article.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_131359) do
+ActiveRecord::Schema.define(version: 2019_09_23_144015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2019_09_23_131359) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
